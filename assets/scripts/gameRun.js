b0fb22bbe1857631f2ab907006a70322dc82da33
Sanjib Roy <sanjibroy0098@gmail.com>
2020-08-12 17:07:43 +0530

    complete

--- assets/scripts/gameRun.js
+++ assets/scripts/gameRun.js
@@ -1,128 +1,128 @@
 function trackKeys(keys) {
-    let down = Object.create(null);
-    function track(event) {
-      if (keys.includes(event.key)) {
-        down[event.key] = event.type == "keydown";
-        event.preventDefault();
-      }
+  let down = Object.create(null);
+  function track(event) {
+    if (keys.includes(event.key)) {
+      down[event.key] = event.type == "keydown";
+      event.preventDefault();
     }
-    window.addEventListener("keydown", track);
-    window.addEventListener("keyup", track);
-    return down;
   }
-  
-  var arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
-  
-  function runAnimation(frameFunc) {
-    let lastTime = null;
-    function frame(time) {
-      if (lastTime != null) {
-        let timeStep = Math.min(time - lastTime, 100) / 1000;
-        if (frameFunc(timeStep) === false) return;
-      }
-      lastTime = time;
-      requestAnimationFrame(frame);
+  window.addEventListener("keydown", track);
+  window.addEventListener("keyup", track);
+  return down;
+}
+
+var arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
+
+function runAnimation(frameFunc) {
+  let lastTime = null;
+  function frame(time) {
+    if (lastTime != null) {
+      let timeStep = Math.min(time - lastTime, 100) / 1000;
+      if (frameFunc(timeStep) === false) return;
     }
+    lastTime = time;
     requestAnimationFrame(frame);
   }
-  
-  function runLevel(level, Display) {
-    let parent = document.getElementById("root");
-    let display = new Display(parent, level);
-    let state = State.start(level);
-    let ending = 1;
-    return new Promise((resolve) => {
-      runAnimation((time) => {
-        state = state.update(time, arrowKeys);
-        display.syncState(state);
-        if (state.status == "playing") {
+  requestAnimationFrame(frame);
+}
+
+function runLevel(level, Display) {
+  let parent = document.getElementById("root");
+  let display = new Display(parent, level);
+  let state = State.start(level);
+  let ending = 1;
+  return new Promise((resolve) => {
+    runAnimation((time) => {
+      state = state.update(time, arrowKeys);
+      display.syncState(state);
+      if (state.status == "playing") {
+        return true;
+      } else if (ending > 0) {
+        setTimeout(function () {
+          ending = -1;
           return true;
-        } else if (ending > 0) {
-          setTimeout(function () {
-            ending = -1;
-            return true;
-          }, 500);
-        } else {
-          display.clear();
-          resolve(state.status);
-          return false;
-        }
-      });
+        }, 500);
+      } else {
+        display.clear();
+        resolve(state.status);
+        return false;
+      }
     });
-  }
-  
-  async function runGame(plans, Display) {
-    let lives = 3;
-    let parent = document.getElementById("root");
-  
-    let scoreBoard = elt("div", { class: "score_board" });
-    scoreBoard.innerHTML = `<div class="score_board">
+  });
+}
+
+async function runGame(plans, Display) {
+  let lives = 3;
+  let parent = document.getElementById("root");
+
+  let scoreBoard = elt("div", { class: "score_board" });
+  scoreBoard.innerHTML = `<div class="score_board">
                               <div class="wrapper">
                                 <p class="lives"></p>
                                 <p class="coin_remaining"></p>
                               </div>
                             </div>`;
-                            
-    parent.appendChild(scoreBoard);
-    let heart = ["❤", "❤ ❤", "❤ ❤ ❤"];
-    let liveElm = document.querySelector(".lives");
-    liveElm.innerText = `Lives: ${heart[lives - 1]}`;
-    for (var level = 0; level < plans.length && lives > 0; ) {
-      let status = await runLevel(new Level(plans[level]), Display);
-      if (status == "won") level++;
-      if (status == "lost") {
-        lives--;
-        lives
-          ? (liveElm.innerText = `Lives: ${heart[lives - 1]}`)
-          : (liveElm.innerText = `Game Over`);
-      }
-    }
-    if (!lives) {
-      gameOver(Display, plans[level]);
-    } else {
-      gameWon(Display, plans[level]);
+
+  parent.appendChild(scoreBoard);
+  let heart = ["❤", "❤ ❤", "❤ ❤ ❤"];
+  let liveElm = document.querySelector(".lives");
+  liveElm.innerText = `Lives: ${heart[lives - 1]}`;
+  for (var level = 0; level < plans.length && lives > 0; ) {
+    let status = await runLevel(new Level(plans[level]), Display);
+    if (status == "won") level++;
+    if (status == "lost") {
+      lives--;
+      lives
+        ? (liveElm.innerText = `Lives: ${heart[lives - 1]}`)
+        : (liveElm.innerText = `Game Over`);
     }
   }
-  
-  function gameOver(Display, plan) {
-    let level = new Level(plan);
-    let parent = document.getElementById("root");
-    parent.classList.add("game_over");
-    let display = new Display(parent, level);
-  
-    let div = `<div class="finish_page_wrapper">
+  if (!lives) {
+    gameOver(Display, plans[level]);
+  } else {
+    gameWon(Display, plans[level]);
+  }
+}
+
+function gameOver(Display, plan) {
+  let level = new Level(plan);
+  let parent = document.getElementById("root");
+  parent.classList.add("game_over");
+  let display = new Display(parent, level);
+
+  let div = `<div class="finish_page_wrapper">
                 <button class="play_btn">Play Again!</button>
               </div>`;
-    parent.innerHTML = div;
-    let restartBtn = document.querySelector(".play_btn");
-    restartBtn.addEventListener("click", (event) => {
-      display.clear();
-      restart();
-    });
-  }
-  
-  
-  function gameWon(Display, plan) {
-    let level = new Level(levelPlan[0]);
-    let parent = document.getElementById("root");
-    parent.classList.add("game_over");
-    let display = new Display(parent, level);
-  
-    let div = `<div class="win_page_wrapper">
+  parent.innerHTML = div;
+  let restartBtn = document.querySelector(".play_btn");
+  restartBtn.addEventListener("click", (event) => {
+    display.clear();
+    restart();
+  });
+}
+
+function gameWon(Display, plan) {
+  let level = new Level(levelPlan[0]);
+  let parent = document.getElementById("root");
+  parent.classList.add("game_over");
+  let display = new Display(parent, level);
+
+  let div = `<div class="win_page_wrapper">
                 <button class="play_btn">Play Again!</button>
               </div>`;
-    parent.innerHTML = div;
-    let restartBtn = document.querySelector(".play_btn");
-    restartBtn.addEventListener("click", (event) => {
-      display.clear();
-      restart();
-    });
-  }
-  
-  function restart() {
-    let resultPage = document.querySelector(".finish_page_wrapper") ||document.querySelector(".win_page_wrapper");
-    resultPage.remove();
-    runGame(levelPlan, DOMDisplay);
-    console.log("Clicked!");
-  }
-  
+  parent.innerHTML = div;
+  let restartBtn = document.querySelector(".play_btn");
+  restartBtn.addEventListener("click", (event) => {
+    display.clear();
+    restart();
+  });
+}
+
+function restart() {
+  let resultPage =
+    document.querySelector(".finish_page_wrapper") ||
+    document.querySelector(".win_page_wrapper");
+  resultPage.remove();
+  runGame(levelPlan, DOMDisplay);
+  console.log("Clicked!");
+}
--- assets/scripts/helperFunctions.js
+++ assets/scripts/helperFunctions.js
@@ -1,82 +1,81 @@
 var levelChars = {
-    ".": "empty",
-    "#": "wall",
-    "+": "lava",
-    "@": Player,
-    o: Coin,
-    "=": Lava,
-    "|": Lava,
-    v: Lava,
-  };
-  
-  function elt(name, attrs, ...children) {
-    let dom = document.createElement(name);
-    for (let attribute in attrs) {
-      dom.setAttribute(attribute, attrs[attribute]);
-    }
-    for (let child of children) {
-      dom.appendChild(child);
-    }
-    return dom;
+  ".": "empty",
+  "#": "wall",
+  "+": "lava",
+  "@": Player,
+  o: Coin,
+  "=": Lava,
+  "|": Lava,
+  v: Lava,
+};
+
+function elt(name, attrs, ...children) {
+  let dom = document.createElement(name);
+  for (let attribute in attrs) {
+    dom.setAttribute(attribute, attrs[attribute]);
   }
-  
-  function drawGrid(level) {
-    return elt(
-      "table",
-      {
-        class: "background",
-        style: `width: ${level.width * scale}px`,
-      },
-      ...level.rows.map((row) =>
-        elt(
-          "tr",
-          { style: `height: ${scale}px` },
-          ...row.map((type) => {
-            if (type === "lava") {
-              return elt("td", { class: type + " static_lava" });
-            } else {
-              return elt("td", { class: type });
-            }
-          })
-        )
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+  return dom;
+}
+
+function drawGrid(level) {
+  return elt(
+    "table",
+    {
+      class: "background",
+      style: `width: ${level.width * scale}px`,
+    },
+    ...level.rows.map((row) =>
+      elt(
+        "tr",
+        { style: `height: ${scale}px` },
+        ...row.map((type) => {
+          if (type === "lava") {
+            return elt("td", { class: type + " static_lava" });
+          } else {
+            return elt("td", { class: type });
+          }
+        })
       )
-    );
-  }
-  
-  function drawActors(actors) {
-    let coinsRemaining = actors.filter((actor) => actor.type === "coin").length;
-    let p = document.querySelector(".coin_remaining");
-    p.innerText = `Coins Remaining: ${coinsRemaining}`;
-  
-    return elt(
-      "div",
-      {},
-      ...actors.map((actor) => {
-        let rect = "";
-  
-        if (actor.type == "lava") {
-          rect = elt("img", { class: `actor ${actor.type} fireball` });
-          rect.setAttribute("src", "./assets/fireball.gif");
-        } else {
-          rect = elt("div", { class: `actor ${actor.type}` });
-        }
-        rect.style.width = `${actor.size.x * scale}px`;
-        rect.style.height = `${actor.size.y * scale}px`;
-        rect.style.left = `${actor.pos.x * scale}px`;
-        rect.style.top = `${actor.pos.y * scale}px`;
-        return rect;
-      })
-    );
-  }
-  
-  function overlap(actor1, actor2) {
-    return (
-      actor1.pos.x + actor1.size.x > actor2.pos.x &&
-      actor1.pos.x < actor2.pos.x + actor2.size.x &&
-      actor1.pos.y + actor1.size.y > actor2.pos.y &&
-      actor1.pos.y < actor2.pos.y + actor2.size.y
-    );
-  }
-  
-  runGame(levelPlan, DOMDisplay);
-  
+    )
+  );
+}
+
+function drawActors(actors) {
+  let coinsRemaining = actors.filter((actor) => actor.type === "coin").length;
+  let p = document.querySelector(".coin_remaining");
+  p.innerText = `Coins Remaining: ${coinsRemaining}`;
+
+  return elt(
+    "div",
+    {},
+    ...actors.map((actor) => {
+      let rect = "";
+
+      if (actor.type == "lava") {
+        rect = elt("img", { class: `actor ${actor.type} fireball` });
+        rect.setAttribute("src", "./assets/fireball.gif");
+      } else {
+        rect = elt("div", { class: `actor ${actor.type}` });
+      }
+      rect.style.width = `${actor.size.x * scale}px`;
+      rect.style.height = `${actor.size.y * scale}px`;
+      rect.style.left = `${actor.pos.x * scale}px`;
+      rect.style.top = `${actor.pos.y * scale}px`;
+      return rect;
+    })
+  );
+}
+
+function overlap(actor1, actor2) {
+  return (
+    actor1.pos.x + actor1.size.x > actor2.pos.x &&
+    actor1.pos.x < actor2.pos.x + actor2.size.x &&
+    actor1.pos.y + actor1.size.y > actor2.pos.y &&
+    actor1.pos.y < actor2.pos.y + actor2.size.y
+  );
+}
+
+runGame(levelPlan, DOMDisplay);
